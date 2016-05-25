7ca2197dcc6f8870b61bcf09a12030ca8205e593
thkwon <kwon@ubuntu.(none)>
2016-05-25 01:07:50 -0700

    Last commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-from .models import Post, Comment
-
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title')
-
-
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    pass
-
